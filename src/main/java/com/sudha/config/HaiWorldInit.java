7b3224fde57ec4befc1aedb3c0e2b1578ce24f33
sudhamaheswari <sudhamaheswariprathipati@gmail.com>
2016-12-22 08:02:59 +0530

    Add files via upload

---
+++ src/main/java/com/sudha/config/HaiWorldInit.java
@@ -0,0 +1,28 @@
+package com.sudha.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+
+public class HaiWorldInit implements WebApplicationInitializer  {
+
+	
+	
+	 public void onStartup(ServletContext servletContext) throws ServletException {
+
+	        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+	        ctx.register(HaiWorldConfig.class);
+	        ctx.setServletContext(servletContext);
+
+	        ServletRegistration.Dynamic servlet = servletContext.addServlet(
+	                "dispatcher", new DispatcherServlet(ctx));
+
+	        servlet.setLoadOnStartup(1);
+	        servlet.addMapping("/");
+	    }
+}
