765d98acec0285d75c1f9ef41310a75c6282fd75
sudhamaheswari <sudhamaheswariprathipati@gmail.com>
2016-12-22 16:16:00 +0530

    Add files via upload

---
+++ commerce/src/test/java/com/mycompany/java/debugjava.java
@@ -0,0 +1,15 @@
+package com.mycompany.java;
+
+public class debugjava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		boolean status = getStatus();
+		System.out.println("Java program status is "+status);
+	}
+
+	static boolean getStatus(){
+		return true;
+	}
+}
